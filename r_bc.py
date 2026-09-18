2a03675bd4982149515123f4eb8bf76e8db43c97
Garv Trivedi <GarvTrivedi6c6f6c@gmail.com>
2026-09-18 11:27:40 +0530

    Update r_bc.py

--- r_bc.py
+++ r_bc.py
@@ -84,4 +84,10 @@ def calculate_r_bc(mdot):
             "for the given mdot."
         )
 
-    return root1, root2
+
+    # Convert from r_ISCO units to R_S units
+    root1_Rs = 3 * root1
+    root2_Rs = 3 * root2
+
+    return root1_Rs, root2_Rs
+
